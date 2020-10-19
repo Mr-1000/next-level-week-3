623f89bc0bf64d951b7d0f14af99ab3a4c81e73c
Mrmaster <Mr_souza21@hotmail.com>
2020-10-19 11:07:34 -0300

    server atualizado

--- NLW3/src/server.js
+++ NLW3/src/server.js
@@ -8,18 +8,15 @@ const pages = require('./pages')
 const server = express();
 
 server
-  //atualizando arquivos estaticos
+      //atualizando arquivos estaticos
   .use(express.static('public'))
-
-  //configurar template engine
+      //configurar template engine
   .set('views', path.join(__dirname, "views"))
-    .set('view engine', 'hbs')
-
-    //cria a rota
+  .set('view engine', 'hbs')
+      //cria a rota
     .get('/', pages.index)
     .get('/orphanage', pages.orphanage)
     .get('/orphanages', pages.orphanages)
     .get('/create-orphanages', pages.createOrphanage)
-
-  //abre o servidor
-  .listen(8080)
+     //abre o servidor
+.listen(8080)
